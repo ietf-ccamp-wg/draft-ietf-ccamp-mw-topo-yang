0fa710243fe6c8dcccb6eda8fe9334eddaaf56ab
samans <scott.mansfield@gmail.com>
2023-09-27 16:48:57 -0400

    last call comment resolution

--- json/fold.sh
+++ json/fold.sh
@@ -1,2 +1,3 @@
-mv example2plus0.json example2plus0-orig.json
 ~/tools/rfcfold -s 1 -c 69 -i example2plus0-orig.json -o example2plus0.json
+~/tools/rfcfold -s 1 -c 69 -i example1plus1-mw-only-orig.json -o example1plus1-mw-only.json
+~/tools/rfcfold -s 1 -c 69 -i example2plus0-mw-only-orig.json -o example2plus0-mw-only.json
